1e71d2586bd057090abd0d8f809c6895b24bf15c
sriram <=>
2013-07-09 16:36:40 +0530

    all414

---
+++ vendor/bundle/ruby/1.9.1/gems/capybara-2.0.3/lib/capybara/helpers.rb
@@ -0,0 +1,32 @@
+# encoding: UTF-8
+
+module Capybara
+  module Helpers
+    class << self
+      ##
+      #
+      # Normalizes whitespace space by stripping leading and trailing
+      # whitespace and replacing sequences of whitespace characters
+      # with a single space.
+      #
+      # @param [String] text     Text to normalize
+      # @return [String]         Normalized text
+      #
+      def normalize_whitespace(text)
+        text.to_s.gsub(/[[:space:]]+/, ' ').strip
+      end
+
+      ##
+      #
+      # Escapes any characters that would have special meaning in a regexp
+      # if text is not a regexp
+      #
+      # @param [String] text Text to escape
+      # @return [String]     Escaped text
+      #
+      def to_regexp(text)
+        text.is_a?(Regexp) ? text : Regexp.escape(normalize_whitespace(text))
+      end
+    end
+  end
+end
---
+++ vendor/bundle/ruby/1.9.1/gems/ffi-1.9.0/ext/ffi_c/libffi-i686-linux/include/ffitarget.h
@@ -0,0 +1,1 @@
+/home/istpl/Documents/redmine-2.3.0/vendor/bundle/ruby/1.9.1/gems/ffi-1.9.0/ext/ffi_c/libffi/src/x86/ffitarget.h
